d3172f7262026d28bb0469246740ae6ef826892e
David Baiguini <david.b@clubware.co.nz>
2021-02-09 19:32:15 +1100

    Handle the PLACE action

--- src/context/robot/robotReducer.test.tsx
+++ src/context/robot/robotReducer.test.tsx
@@ -1,5 +1,10 @@
 import { ACTION_TYPE, FACING, TRobotReducerActions } from './RobotContext';
-import { robotReducer, isValidPosition } from './robotReducer';
+import {
+  robotReducer,
+  isValidPosition,
+  isRobotPlaced,
+  calculateNextPosition,
+} from './robotReducer';
 
 describe('validatePosition', () => {
   it('should return true when the position is valid', () => {
@@ -15,7 +20,38 @@ describe('validatePosition', () => {
     expect(isValidPosition([10, 0], 10)).toBe(false);
     expect(isValidPosition([5, 5], 5)).toBe(false);
     expect(isValidPosition([0, 0], 0)).toBe(false);
+    // @ts-expect-error
     expect(isValidPosition([undefined, 0], 5)).toBe(false);
+  });
+});
+
+describe('isRobotPlaced', () => {
+  it('should return true when the robot was placed', () => {
+    expect(isRobotPlaced([0, 0], FACING.south)).toBe(true);
+    expect(isRobotPlaced([0, 0], FACING.east)).toBe(true);
+    expect(isRobotPlaced([0, 0], FACING.west)).toBe(true);
+    expect(isRobotPlaced([99, 1], FACING.south)).toBe(true);
+  });
+  it('should return false when the position is not valid', () => {
+    expect(isRobotPlaced([0, 0], undefined)).toBe(false);
+    expect(isRobotPlaced(undefined, FACING.south)).toBe(false);
+    // @ts-expect-error
+    expect(isRobotPlaced([0, 0], 'SOUTHHHHHHH')).toBe(false);
+    // @ts-expect-error
+    expect(isRobotPlaced(1, FACING.south)).toBe(false);
+  });
+});
+
+describe('calculateNextPosition', () => {
+  it('should correctly calculate the next position of the robot', () => {
+    expect(calculateNextPosition([0, 0], FACING.north)).toEqual([0, 1]);
+    expect(calculateNextPosition([10, 5], FACING.north)).toEqual([10, 6]);
+    expect(calculateNextPosition([10, 10], FACING.south)).toEqual([10, 9]);
+    expect(calculateNextPosition([10, 10], FACING.west)).toEqual([9, 10]);
+    expect(calculateNextPosition([10, 10], FACING.east)).toEqual([11, 10]);
+  });
+  it('should throw when the parameters are not valid', () => {
+    expect(() => calculateNextPosition(undefined, FACING.north)).toThrow();
   });
 });
 
@@ -32,6 +68,43 @@ describe('robotReducer', () => {
         boardSize: 5,
         position: [1, 2],
         facing: 'EAST',
+      });
+    });
+  });
+
+  describe('The MOVE action', () => {
+    it('Should move the robot', () => {
+      const action: TRobotReducerActions = {
+        type: ACTION_TYPE.move,
+      };
+      const state = {
+        boardSize: 5,
+        facing: FACING.north,
+        position: [0, 0] as [number, number],
+      };
+      const newState = robotReducer(state, action);
+
+      expect(newState).toStrictEqual({
+        boardSize: 5,
+        position: [0, 1],
+        facing: 'NORTH',
+      });
+    });
+    it('should not move the robot if the new position is invalid', () => {
+      const action: TRobotReducerActions = {
+        type: ACTION_TYPE.move,
+      };
+      const state = {
+        boardSize: 5,
+        facing: FACING.south,
+        position: [0, 0] as [number, number],
+      };
+      const newState = robotReducer(state, action);
+
+      expect(newState).toStrictEqual({
+        boardSize: 5,
+        position: [0, 0],
+        facing: 'SOUTH',
       });
     });
   });
--- src/context/robot/robotReducer.ts
+++ src/context/robot/robotReducer.ts
@@ -2,10 +2,21 @@ import { isNil } from 'ramda';
 
 import {
   ACTION_TYPE,
+  FACING,
   TRobotContext,
   TRobotReducerActions,
 } from './RobotContext';
 
+const DIRECTION = {
+  NORTH: [0, 1],
+  SOUTH: [0, -1],
+  EAST: [1, 0],
+  WEST: [-1, 0],
+};
+
+/**
+ * Check if the position is valid
+ */
 export const isValidPosition = (
   position?: [number, number],
   boardSize?: number
@@ -18,6 +29,49 @@ export const isValidPosition = (
     return false;
   }
   return true;
+};
+
+/**
+ * Check if the robot is present on the board
+ */
+export const isRobotPlaced = (
+  position?: [number, number],
+  facing?: FACING
+): boolean => {
+  if (Array.isArray(position) && position.length === 2) {
+    const [x, y] = position;
+    if (!isValidFacing(facing) || isNil(x) || isNil(y)) {
+      return false;
+    }
+    return true;
+  }
+
+  return false;
+};
+
+/**
+ * Type Guard and check that `facing` has a correct value
+ */
+const isValidFacing = (facing?: FACING): facing is FACING => {
+  if (!facing) {
+    return false;
+  }
+  return Object.values(FACING).indexOf(facing) !== -1;
+};
+
+/**
+ * Calculate the next position based on the current position and what direction the robot is facing
+ */
+export const calculateNextPosition = (
+  position?: [number, number],
+  facing?: FACING
+): [number, number] => {
+  if (!position || !facing) {
+    throw new Error("Couldn't calculate the next position");
+  }
+  const x = position[0] + DIRECTION[facing][0];
+  const y = position[1] + DIRECTION[facing][1];
+  return [x, y];
 };
 
 export const robotReducer = (
@@ -36,6 +90,19 @@ export const robotReducer = (
         facing,
         position: [x, y],
       };
+    }
+
+    case ACTION_TYPE.move: {
+      const { position, facing, boardSize } = state;
+      if (!isRobotPlaced(position, facing)) {
+        return state;
+      }
+      const nextPosition = calculateNextPosition(position, facing);
+      if (!isValidPosition(nextPosition, boardSize)) {
+        return state;
+      }
+
+      return { ...state, position: nextPosition };
     }
 
     default:
